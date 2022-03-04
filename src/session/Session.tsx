3c219349c4f1d550ec099a4fce092b9b920932b0
Jakob Hansen <jakob.hansen@hotmail.no>
2022-03-04 22:37:35 +0100

    Search parameters

--- src/session/Session.tsx
+++ src/session/Session.tsx
@@ -1,3 +1,9 @@
+import { useSearchParams } from "react-router-dom"
+
+
 export function Session() {
+  let [searchParams, setSearchParams] = useSearchParams();
+  const username = searchParams.get("username")
+  const sessionId = searchParams.get("sessionId")
   return <div>Session!</div>
 }
