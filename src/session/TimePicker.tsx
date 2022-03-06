40163c775a126cd3f7ee97491886bbdaf344a7bb
Jakob Hansen <jakob.hansen@hotmail.no>
2022-03-06 21:35:13 +0100

    Better looking css TimePicker

--- src/session/TimePicker.tsx
+++ src/session/TimePicker.tsx
@@ -3,7 +3,7 @@ import { TimePickerOption } from "./TimePickerOption";
 
 export function TimePicker() {
   return (
-    <div className="w-1/3 m-auto h-screen flex flex-row justify-center content-center flex-wrap">
+    <div className="max-h-screen max-w-xl m-auto h-screen flex flex-row justify-around content-center flex-wrap">
       {defaultTimeOptions.map((secs, i) => {
         return <TimePickerOption key={i} seconds={secs} />;
       })}
