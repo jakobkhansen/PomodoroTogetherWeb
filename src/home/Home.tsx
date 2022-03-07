79fc418d7c0ce9a7e1c41180415249f78a50892b
Jakob Hansen <jakob.hansen@hotmail.no>
2022-03-07 14:34:08 +0100

    Revert "Dark"
    This reverts commit 2a064ed3d27ab18a1a986dab219121cadcb629ea.

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,20 +1,13 @@
 import header from "resources/5834.jpg";
-import { ThemeProvider } from "utils/ThemeContext";
-import DarkModeToggle from "./DarkModeToggle";
 import { HomeInputForm } from "./HomeInputForm";
 
 function Home() {
   return (
-    <ThemeProvider>
-      <div className="h-screen bg-white dark:bg-gray-700 duration-1000">
-      <div className="m-auto text-center max-w-4xl">
-        <DarkModeToggle />
-          <h1 className="m-auto text-4xl pt-5 mb-5 dark:text-white duration-1000">Pomodoro Together</h1>
-        <img src={header} className="w-4/5 m-auto" />
-        <HomeInputForm />
-      </div>
-      </div>
-    </ThemeProvider>
+    <div className="m-auto text-center max-w-4xl">
+      <h1 className="m-auto text-4xl mt-5 mb-5">Pomodoro Together</h1>
+      <img src={header} className="w-4/5 m-auto"/>
+      <HomeInputForm />
+    </div>
   );
 }
 
