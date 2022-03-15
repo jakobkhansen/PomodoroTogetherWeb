09d086d2d7ccfa3695c5fb0080baf73f61356917
Jakob Hansen <jakob.hansen@hotmail.no>
2022-03-15 19:54:39 +0100

    Use grid for TimePicker

--- src/session/TimePicker.tsx
+++ src/session/TimePicker.tsx
@@ -2,12 +2,14 @@ import { defaultTimeOptions } from "utils";
 import { TimePickerOption } from "./TimePickerOption";
 
 export function TimePicker() {
-  document.title = "Pomodoro Together"
+  document.title = "Pomodoro Together";
   return (
-    <div className="max-h-screen max-w-xl m-auto h-screen flex flex-row justify-around content-center flex-wrap">
-      {defaultTimeOptions.map((secs, i) => {
-        return <TimePickerOption key={i} seconds={secs} />;
-      })}
+    <div className="w-full h-screen m-auto flex">
+      <div className="w-fit m-auto grid grid-cols-4">
+        {defaultTimeOptions.map((secs, i) => {
+          return <TimePickerOption key={i} seconds={secs} />;
+        })}
+      </div>
     </div>
   );
 }
