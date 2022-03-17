41598f8071782a25d3029b0b3deb77512df4436c
Jakob Hansen <jakob.hansen@hotmail.no>
2022-03-17 21:50:17 +0100

    Scrollable and responsive TimePicker

--- src/session/TimePicker.tsx
+++ src/session/TimePicker.tsx
@@ -4,8 +4,8 @@ import { TimePickerOption } from "./TimePickerOption";
 export function TimePicker() {
   document.title = "Pomodoro Together";
   return (
-    <div className="w-full h-screen m-auto flex">
-      <div className="w-fit m-auto sm:grid-cols-2 md:grid-cols-4 gap-2">
+    <div className="w-full h-screen overflow-y-scroll m-auto flex">
+      <div className="w-fit m-auto my-4 sm:my-auto grid grid-cols-2 sm:grid-cols-4 gap-2">
         {defaultTimeOptions.map((secs, i) => {
           return <TimePickerOption key={i} seconds={secs} />;
         })}
