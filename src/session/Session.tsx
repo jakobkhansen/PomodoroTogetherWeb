7d7c9b58e1bc064b77e30d2922cb6abf9cb8f38f
Jakob Hansen <jakob.hansen@hotmail.no>
2022-03-24 11:56:45 +0100

    Prevent sleeping attempt 1

--- src/session/Session.tsx
+++ src/session/Session.tsx
@@ -1,10 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
-import {
-    useLocation,
-    useNavigate,
-    useParams
-} from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import useStayAwake from "use-stay-awake";
 import { getDateSeconds, PomodoroState } from "utils";
 import { SessionState } from "utils/SessionState";
 import { SocketContext } from "utils/SocketContext";
@@ -16,6 +13,7 @@ export function Session() {
   const location = useLocation();
   const navigate = useNavigate();
   const [cookie, setCookie] = useCookies(["displayName", "sessionName"]);
+  const device = useStayAwake();
 
   const sessionName = useParams().sessionName || "";
   setCookie("sessionName", sessionName, {
@@ -57,6 +55,14 @@ export function Session() {
       socket?.sendLeave();
     };
   }, [socket]);
+
+  useEffect(() => {
+    device.preventSleeping();
+
+    return function () {
+      device.allowSleeping();
+    };
+  });
 
   function renderIfReady(): React.ReactElement {
     if (sessionState?.clock.state == PomodoroState.DONE) {
