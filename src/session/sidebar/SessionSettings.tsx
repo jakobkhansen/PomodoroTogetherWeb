fb5ab4aec4d4066ea28820e1d03b50665f0a64a7
Jakob Hansen <jakob.hansen@hotmail.no>
2022-04-01 20:57:17 +0200

    Stop timer and leave session

--- src/session/sidebar/SessionSettings.tsx
+++ src/session/sidebar/SessionSettings.tsx
@@ -1,11 +1,32 @@
 import { Button, List, ListItem } from "@mui/material";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { SocketContext } from "utils/SocketContext";
+
+type SessionSettingsProps = {
+  stopTimer: () => void | void;
+};
 
 export function SessionSettings() {
-  return <List>
-    <ListItem>
-      <Button>
-        Stop timer
-      </Button>
-    </ListItem>
-  </List>
+  const [socket] = useContext(SocketContext);
+  const navigate = useNavigate();
+
+  function stopTimer() {
+    socket?.sendStop();
+  }
+
+  function leaveSession() {
+    navigate("/");
+  }
+
+  return (
+    <List>
+      <ListItem alignItems="center">
+        <Button onClick={stopTimer}>Stop timer</Button>
+      </ListItem>
+      <ListItem alignItems="center">
+        <Button onClick={leaveSession}>Leave session</Button>
+      </ListItem>
+    </List>
+  );
 }
